76a05e6c58dd216b80164186163ed7e193b31e97
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-14 20:04:48 +0000

    ALL MY SECRETS AWAY

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function markAsDone(todos) {
 function transform(index, todos, modifiedTodos){
     if(index < todos.length){
         // on this line - prepend the string 'done - ' to each `todo` description
-        updated.push('done - ' + todo)
+        updated.push('done - ' + todos)
         // on this line - call transform recursively
         todos += index;
         index ++;
