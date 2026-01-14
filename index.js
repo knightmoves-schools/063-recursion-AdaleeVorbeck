6c7abc1322be0afaab228c504b4bcba0e8044346
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-14 20:01:41 +0000

    Added some new songs so lets hope it still a good playlist

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ function markAsDone(todos) {
 function transform(index, todos, modifiedTodos){
     if(index < todos.length){
         // on this line - prepend the string 'done - ' to each `todo` description
+        updated.push('done - ' + todo)
         // on this line - call transform recursively
+        todos += index;
+        index ++;
         return modifiedTodos
     }else{
         return modifiedTodos
